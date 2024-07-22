d8b44891ca94eb038b43dcd77e557d534d8c2d0f
maheshsanam <maheshwarasudharsanam1259@gamil.com>
2024-07-22 12:50:51 +0530

    mnb

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-
-
+@app.route("/restaurant")
+def restaurant():
+    return render_template("restaurant.html")
 
 
 if __name__=='__main__':
     app.run()
+
